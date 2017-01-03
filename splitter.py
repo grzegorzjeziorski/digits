c6e205fd78d0b2dd115e8338cbfbe844f64b74e2
gjeziorski <grzesiek.jeziorski@gmail.com>
2017-01-03 10:44:35 +0100

    no dropout possible

--- splitter.py
+++ splitter.py
@@ -16,10 +16,11 @@ def build_args_parser():
 def cli(cli_args):
     parser = build_args_parser()
     args = parser.parse_args(cli_args)
-    if args.file and args.train and args.test and args.percentage and args.dropout:
+    if args.file and args.train and args.test and args.percentage:
         df = pd.read_csv(args.file)
-        new_size = len(df.index) * (100 - args.dropout) / 100        
-        df = df[0:new_size]
+        if args.dropout:
+            new_size = len(df.index) * (100 - args.dropout) / 100        
+            df = df[0:new_size]
         train_df, test_df = train_test_split(df, test_size=float(args.percentage)/float(100), random_state=1)
         train_df.to_csv(args.train, index=False)
         test_df.to_csv(args.test, index=False)
